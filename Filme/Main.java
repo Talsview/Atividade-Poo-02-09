dbce91e229134cf77d054f263ef645c4fbe4bbc4
Talsview <daviconcyline@gmail.com>
2025-09-08 18:54:03 -0300

    Main.java

--- Filme/Main.java
+++ Filme/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        
         Filme filme = new Filme("Um Monstro em Paris", "Bibo Bergeron", 2011, 1.3);
+        
         filme.exibirDetalhes();
         filme.verificarFilmeClassico();
     }
 
 }
+
